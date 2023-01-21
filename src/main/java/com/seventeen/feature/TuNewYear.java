54b82a2a5be7174a593762ff29913b7c14f148d7
pengkun.wen <wensir_top@163.com>
2023-01-21 20:43:39 +0800

    Happy new year

---
+++ src/main/java/com/seventeen/feature/TuNewYear.java
@@ -0,0 +1,11 @@
+package com.seventeen.feature;
+
+public class TuNewYear {
+    public static void main(String[] args) {
+        System.out.println("""
+                Happy
+                New
+                Year
+                """);
+    }
+}
