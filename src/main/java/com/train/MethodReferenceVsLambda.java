304c772d6be09264f7745ef40ee59161f5edba1c
= <=>
2024-01-08 10:40:23 +0800

    Add step comment

--- src/main/java/com/train/MethodReferenceVsLambda.java
+++ src/main/java/com/train/MethodReferenceVsLambda.java
@@ -2,15 +2,20 @@ package com.train;
 
 public class MethodReferenceVsLambda {
     public static void main(String[] args) {
+        //lazy initialize
         Runnable universelImpactRunnable = () -> new ChuckNorris().roundHouseKick();
         System.out.println("-----------------------");
+        /*
+         * Will invoke make numberKicks to 1
+         * and galaxyDamage = 0;
+         */
         Runnable galaxyImpactRunnable = new ChuckNorris()::roundHouseKick;
         System.out.print("The galaxy is finished = ");
-        universelImpactRunnable.run();
-        universelImpactRunnable.run();
+        universelImpactRunnable.run(); // will invoke constructor galaxyDamage = 1; numberKicks to 2
+        universelImpactRunnable.run(); // will invoke constructor galaxyDamage = 2; numberKicks to 3
 
-        galaxyImpactRunnable.run();
-        galaxyImpactRunnable.run();
+        galaxyImpactRunnable.run(); //numberKicks is 0
+        galaxyImpactRunnable.run();//numberKicks is 0
     }
 
     static class ChuckNorris {
